a6ab0337a0cb7d3abcc11bc7d7b8490c869f8927
imrany <imranmat254@gmail.com>
2024-07-27 19:48:59 +0300

    not supported component

---
+++ src/components/NotSupported.tsx
@@ -0,0 +1,7 @@
+export default function NotSupported(){
+    return(
+        <div className="bg-[var(--primary-01)] text-[var(--primary-02)] h-screen">
+            <p>Not supported</p>
+        </div>
+    )
+}
