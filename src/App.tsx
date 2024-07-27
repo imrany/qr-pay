8070dae85d4ec390bd5f4d13cb7bab291b8d255b
imrany <imranmat254@gmail.com>
2024-07-27 19:49:43 +0300

    implementing not support

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Pay from "./pages/Pay";
 import NotFound from "./pages/NotFound";
+import NotSupported from "./components/NotSupported";
 
 function App() {
+    const [isSupported,setIsSupported]=useState(false)
+    window.onresize=function(){
+        if(screen.width>425){
+            //not supported
+            setIsSupported(false)
+        }else{
+            //supported
+            setIsSupported(true)
+        }
+    }
+
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    </BrowserRouter>
+    <>
+        {isSupported?(
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/pay" element={<Pay/>}/>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        ):(
+            <NotSupported/>    
+        )}
+    </>
   )
 }
 
