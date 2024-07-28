93520c3b89a74970b78969d61fc9e35ad7595cd0
imrany <imranmat254@gmail.com>
2024-07-28 09:51:20 +0300

    added handleScan function

--- src/pages/Pay.tsx
+++ src/pages/Pay.tsx
@@ -1,11 +1,14 @@
 import { Scanner } from '@yudiel/react-qr-scanner';
 
 export default function Pay(){
+    fucntion handleScan(result:any){
+        console.log(result)
+    }
     return(
         <div className="text-white">
             <Scanner
                 classNames={{container:"my-[25vh]"}} 
-                onScan={(result) => alert(result)} 
+                onScan={(result) => handleScan(result)} 
             />
         </div>
     )
