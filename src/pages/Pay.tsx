6ac5edd7daa7213d90bcaa832237fbe974c70d21
imrany <imranmat254@gmail.com>
2024-07-28 10:05:40 +0300

    qr code scanner works

--- src/pages/Pay.tsx
+++ src/pages/Pay.tsx
@@ -2,11 +2,18 @@ import { Scanner } from '@yudiel/react-qr-scanner';
 
 export default function Pay(){
     function handleScan(result:any){
-        console.log(result)
+        result.forEach((i)=>{
+            if(i.format.includes("qr_code")){
+                console.log(i.rawValue)
+            }else{
+                console.log(`${i.format} is not supported`,i.rawValue)
+            }
+        })
     }
     return(
         <div className="text-white">
             <Scanner
+                //allowMultiple={true}
                 classNames={{container:"my-[25vh]"}} 
                 onScan={(result) => handleScan(result)} 
             />
